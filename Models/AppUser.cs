d15bb806f7bcf32783c9cca6c28306dd60454c26
ceydapak <ceydapak@CEYDAPAK>
2023-10-24 17:04:55 +0300

    Proje dosyası ekle.

---
+++ Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Blog_WebSite.Models
+{
+    public class AppUser:IdentityUser
+    {
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public List<Post>? Posts { get; set; }
+    }
+}
