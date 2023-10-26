9b0e7c6f728d85c330abce48e5141c167228fc08
ceydapak <pakceyda@outlook.com>
2023-10-27 00:02:27 +0300

    pagecontroller-bootstrap-pageviews

---
+++ Controllers/PageController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog_WebSite.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult PrivacyP()
+        {
+            return View();
+        }
+    }
+}
